5f934ed84cd586b3e873c68c8aded3922455be7c
destroyergod001-boop <destroyergod001@gmail.com>
2025-08-18 11:58:49 +0600

    Updated ElementsInputGroup.tsx

--- src/Modules/Admin/AdminCommonComponents/ElementsInputGroup.tsx
+++ src/Modules/Admin/AdminCommonComponents/ElementsInputGroup.tsx
@@ -84,7 +84,7 @@ export const ElementsInputGroup: React.FC<ElementsInputGroupProps> = ({
     )}
   </div>
 
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-4">
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
     {elements.map((element) => {
       const value = currentElements[element.name] || 0;
       return (
